2904dc72eddc20e257f2a2f6adb50b88244678d5
Thomas Schaub <thomas.schaub@nettimelogic.com>
2019-09-02 14:04:59 +0200

    Cleanup sdk microblaze cmake

--- Src/SdkMb.cmake
+++ Src/SdkMb.cmake
@@ -8,13 +8,10 @@ set (CMAKE_ASM_COMPILER     "C:/Xilinx/SDK/2019.1/gnu/microblaze/nt/bin/mb-gcc.e
 set (CMAKE_AR               "C:/Xilinx/SDK/2019.1/gnu/microblaze/nt/bin/mb-ar.exe" CACHE FILEPATH "Archiver")
 
 set (UA_ARCHITECTURE "freertosLWIP" CACHE STRING "Architecture to build open62541 on")
-set(UA_ENABLE_AMALGAMATION ON CACHE STRING "" FORCE)
+set (UA_ENABLE_AMALGAMATION ON CACHE STRING "" FORCE)
+set (UA_ENABLE_HARDENING OFF CACHE STRING "" FORCE)
 
-set(UA_ENABLE_AMALGATION ON CACHE STRING "" FORCE)
-set(UA_ENABLE_HARDENING OFF CACHE STRING "" FORCE)
-
-
-SET (UA_ARCH_EXTRA_INCLUDES "C:/NetTimeLogic/TimeSync/CLK/Refdesign/Xilinx/Arty/ClkAllInOneMicroblaze/ClkAllInOneMicroblaze/ClkAllInOneMicroblaze.sdk/Open62541Server_bsp/microblaze_0/include" CACHE STRING "" FORCE)
+SET (UA_ARCH_EXTRA_INCLUDES "C:/NetTimeLogic/TimeSync/CLK/Refdesign/Xilinx/Arty/Open62541/MicroblazeArtyA7/MicroblazeArtyA7.sdk/OpcServer_bsp/microblaze_0/include" CACHE STRING "" FORCE)
 
 SET (UA_ARCH_REMOVE_FLAGS "-Wpedantic -Wno-static-in-inline -Wredundant-decls" CACHE STRING "" FORCE)
 SET (CMAKE_C_FLAGS "-Wno-error=format= -mlittle-endian -DconfigUSE_PORT_OPTIMISED_TASK_SELECTION=0 -DconfigAPPLICATION_ALLOCATED_HEAP=3 -DUA_ARCHITECTURE_FREERTOSLWIP" CACHE STRING "" FORCE)
